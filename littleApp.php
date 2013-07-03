8b40ed5fffb74d582bfb40fc15eac6705e5df229
Ian Anderson Gray <ian@iag.me>
2013-07-03 11:21:00 +0100

    Created littleApp

---
+++ littleApp.php
@@ -0,0 +1,28 @@
+<?php
+require_once('TwitterAPIExchange.php');
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+'oauth_access_token' => "YOUR_OAUTH_ACCESS_TOKEN",
+'oauth_access_token_secret' => "YOUR_OAUTH_ACCESS_TOKEN_SECRET",
+'consumer_key' => "YOUR_CONSUMER_KEY",
+'consumer_secret' => "YOUR_CONSUMER_SECRET"
+);
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+$requestMethod = "GET";
+if (isset($_GET['user'])) {$user = $_GET['user'];} else {$user = "iagdotme";}
+$getfield = '?username=iagdotme&count=20';
+$twitter = new TwitterAPIExchange($settings);
+$string = json_decode($twitter->setGetfield($getfield)
+->buildOauth($url, $requestMethod)
+->performRequest(),$assoc = TRUE);
+foreach($string as $items)
+    {
+        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+        echo "Tweet: ". $items['text']."<br />";
+        echo "Tweeted by: ". $items['user']['name']."<br />";
+        echo "Screen name: ". $items['user']['screen_name']."<br />";
+        echo "Followers: ". $items['user']['followers_count']."<br />";
+        echo "Friends: ". $items['user']['friends_count']."<br />";
+        echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+    }
+?>
